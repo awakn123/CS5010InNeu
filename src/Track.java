e18a50a5547488fe1459201ce7cee855507f47b5
caoyun <awakn123@gmail.com>
2022-10-04 15:27:09 -0400

    add getBirdList function.

--- src/Track.java
+++ src/Track.java
@@ -1,7 +1,12 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Track {
     private Map<Integer, Bird> birdMap = new HashMap<>();
 
+    public List<Bird> getBirdList() {
+        return birdMap.values().stream().collect(Collectors.toList());
+    }
 }
