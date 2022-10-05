331cedc008f43d8d0b634e58e13153d541b3ae0c
caoyun <awakn123@gmail.com>
2022-10-05 09:14:01 -0400

    add Track Test.

---
+++ test/TrackTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TrackTest {
+
+    private int eagleId;
+    private int parrotId;
+    private int waterBirdId;
+    @Test
+    public void getBirdList() {
+    }
+
+    @Test
+    public void getBird() {
+    }
+
+    @Test
+    public void observeNewBird() {
+    }
+
+    @Test
+    public void markParrotWords() {
+    }
+
+    @Test
+    public void markParrotFavorite() {
+    }
+
+    @Test
+    public void markEating() {
+    }
+
+    @Test
+    public void markWaterBody() {
+    }
+}
