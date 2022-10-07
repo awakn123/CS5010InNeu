8fc6705b2f69e96f5b2afc0e99473708c0a7a050
caoyun <awakn123@gmail.com>
2022-10-06 21:18:19 -0400

    add constructor.

--- src/ShoreBird.java
+++ src/ShoreBird.java
@@ -2,4 +2,8 @@ public class ShoreBird extends WaterBird{
     public ShoreBird(BirdType birdType) {
         super(birdType);
     }
+
+    public ShoreBird(String name, BirdType birdtype) {
+        super(name, birdtype);
+    }
 }
